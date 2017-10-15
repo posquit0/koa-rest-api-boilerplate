e9193e94e4fcb36b3eae936f3678a93b5f48b144
Posquit0.BJ <posquit0.bj@gmail.com>
2017-10-15 16:23:59 +0900

    Update integration test cases

--- test/integration/home.test.js
+++ test/integration/home.test.js
@@ -3,11 +3,12 @@
 const supertest = require('supertest');
 const app = require('../../app');
 
+
 describe('Home', () => {
   const request = supertest(app.listen());
 
   describe('GET /', () => {
-    it('should always success with the API server information', async () => {
+    it('<200> should always return with the API server information', async () => {
       const res = await request
         .get('/')
         .expect('Content-Type', /json/)
@@ -18,6 +19,22 @@ describe('Home', () => {
       expect(status).toBe('success');
       expect(message).toBe('Hello, API!');
       expect(Object.keys(data)).toEqual(expect.arrayContaining(expected));
+    });
+  });
+
+  describe('GET /spec', () => {
+    it('<200> should always return API specification in swagger format', async () => {
+      const res = await request
+        .get('/spec')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      const spec = res.body;
+      expect(spec).toHaveProperty('info');
+      expect(spec).toHaveProperty('swagger', '2.0');
+      expect(spec).toHaveProperty('consumes');
+      expect(spec).toHaveProperty('produces');
+      expect(spec).toHaveProperty('paths');
     });
   });
 });
