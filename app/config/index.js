fd9ef829f7c20967ef9a985c2ecfeb5ac6a19c98
Posquit0.BJ <posquit0.bj@gmail.com>
2018-03-22 18:17:29 +0900

    Fix typo in config

--- app/config/index.js
+++ app/config/index.js
@@ -10,7 +10,7 @@ const env = process.env.NODE_ENV || 'development';
 const configs = {
   base: {
     env,
-    name: process.env.APP_NAME || 'image-api',
+    name: process.env.APP_NAME || 'koa-rest-api-boilerplate',
     host: process.env.APP_HOST || '0.0.0.0',
     port: 7070
   },
