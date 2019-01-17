8473ba0118123625dd2e374e58283da7477edf78
Byungjin Park <posquit0.bj@gmail.com>
2019-01-17 18:38:24 +0900

    Divide jest configuration file from `package.json`

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'node',
+  // Exit the test suite immediately upon the first failing test suite
+  bail: true,
+  setupTestFrameworkScriptFile: 'jest-extended'
+};
