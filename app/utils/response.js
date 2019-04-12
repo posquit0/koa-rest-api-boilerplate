4b950c69c67b5186ceeb443cab75e463150dbb95
Byungjin Park <posquit0.bj@gmail.com>
2019-04-12 13:18:18 +0900

    Update response util

--- app/utils/response.js
+++ app/utils/response.js
@@ -1,6 +1,33 @@
 'use strict';
 
 
+/**
+ * HTTP Status codes
+ */
+const STATUS_CODES = {
+  CONTINUE: 100,
+  OK: 200,
+  CREATED: 201,
+  ACCEPTED: 202,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  NOT_ACCEPTABLE: 406,
+  REQUEST_TIMEOUT: 408,
+  CONFLICT: 409,
+  REQUEST_ENTITY_TOO_LARGE: 413,
+  UNSUPPORTED_MEDIA_TYPE: 415,
+  UNPROCESSABLE_ENTITY: 422,
+  TOO_MANY_REQUESTS: 429,
+  INTERNAL_SERVER_ERROR: 500,
+  NOT_IMPLEMENTED: 501,
+  BAD_GATEWAY: 502,
+  SERVICE_UNAVAILABLE: 503,
+  GATEWAY_TIMEOUT: 504
+};
+
 const toResponse = (statusCode, params = {}) => {
   const { code = null, data = null, message = null } = params;
 
@@ -24,33 +51,8 @@ const toResponse = (statusCode, params = {}) => {
  * Utility Class to easily make Koa Response
  */
 class Response {
-  /**
-   * HTTP Status codes
-   */
   static get STATUS_CODES() {
-    return {
-      CONTINUE: 100,
-      OK: 200,
-      CREATED: 201,
-      ACCEPTED: 202,
-      NO_CONTENT: 204,
-      BAD_REQUEST: 400,
-      UNAUTHORIZED: 401,
-      FORBIDDEN: 403,
-      NOT_FOUND: 404,
-      NOT_ACCEPTABLE: 406,
-      REQUEST_TIMEOUT: 408,
-      CONFLICT: 409,
-      REQUEST_ENTITY_TOO_LARGE: 413,
-      UNSUPPORTED_MEDIA_TYPE: 415,
-      UNPROCESSABLE_ENTITY: 422,
-      TOO_MANY_REQUESTS: 429,
-      INTERNAL_SERVER_ERROR: 500,
-      NOT_IMPLEMENTED: 501,
-      BAD_GATEWAY: 502,
-      SERVICE_UNAVAILABLE: 503,
-      GATEWAY_TIMEOUT: 504
-    };
+    return STATUS_CODES;
   }
 
   static success(ctx, params = {}) {
