8ad475f1e29e1da014231472826c20b79a1942a2
Byungjin Park <posquit0.bj@gmail.com>
2019-07-07 23:15:07 +0900

    Add http client util based on got package

---
+++ app/utils/http.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const { isString } = require('util');
+const got = require('got');
+const debug = require('debug')('api:http');
+
+
+class HttpApi {
+  constructor(options = {}) {
+    this.options = {
+      json: true,
+      http: got,
+      ...options
+    };
+
+    debug('Create an instance');
+
+    const baseUrl = this.options.baseUrl;
+    if (!(baseUrl && isString(baseUrl))) {
+      throw new TypeError('`baseUrl` is required.');
+    }
+
+    debug(`baseUrl: ${baseUrl}`);
+
+    const { http, ...httpOptions } = this.options;
+    this.http = http.extend(httpOptions);
+
+    debug('Configured HTTP client');
+  }
+
+  async _request(spec) {
+    const { path, ...options } = spec;
+    debug(`request: ${spec.method} ${path} (${this.options.baseUrl})`);
+
+    const { statusCode, body } = await this.http(path, options);
+    debug(`response: ${statusCode}`);
+    return body;
+  }
+
+  _checkParams(params, required = []) {
+    const isSufficient = required.every(
+      param => Object.hasOwnProperty.call(params, param)
+    );
+    if (!isSufficient) {
+      throw new Error(`Required Parameters: ${required}`);
+    }
+  }
+}
+
+module.exports = HttpApi;
