5fba2e02e64a1ca4e573f54c1254b9dea2b76c19
Byungjin Park <posquit0.bj@gmail.com>
2019-07-07 18:11:00 +0900

    Update apm agent version and configuration

--- app/apm.js
+++ app/apm.js
@@ -10,8 +10,10 @@ apm.start({
     '/',
     '/spec'
   ],
-  // Only activate the agent if it's running in production and apm server is set
-  active: process.env.NODE_ENV === 'production' && !!process.env.ELASTIC_APM_SERVER_URL
+  // Use the URL path as the transaction name if no other route could be determined
+  usePathAsTransactionName: true,
+  // Only activate the agent if the apm server is set
+  active: !!process.env.ELASTIC_APM_SERVER_URL
 });
 
 // Add this to the VERY top of the first file loaded in your app
